54fcb5eee4c28ccf460ef60e822b8f1cc6cd608e
Vitaliyvin <vitaliybondarenk@gmail.com>
2016-01-20 16:00:18 +0200

    test for convert is true

--- NumberImaginationTests/ConvertorTest.cs
+++ NumberImaginationTests/ConvertorTest.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NumberImagination.ConverterLib;
 
 namespace NumberImaginationTests
 {
     [TestClass]
-    class ConvertorTest
+    public class ConvertorTest
     {
         [TestMethod]
         public void ConvetToSTDTestOne()
