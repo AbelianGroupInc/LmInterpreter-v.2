310de4bf83539fb88c6442431f6b8e8ade09b307
Grigoryan Artem <artem.thebest@mail.ru>
2016-01-21 13:44:51 +0200

    Finished the output system

---
+++ LmInterpreterLib/OutputSystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LmInterpreterLib
+{
+    class OutputSystem
+    {
+        private List<IOutputField> mOutputField;
+
+        public void Add(IOutputField outputField)
+        {
+            mOutputField.Add(outputField);
+        }
+
+        public void Remove(IOutputField outputField)
+        {
+            mOutputField.Remove(outputField);
+        }
+
+        public void Clear()
+        {
+            foreach (var currentOutputField in mOutputField)
+                currentOutputField.Clear();
+        }
+
+        public void Write(string buffer)
+        {
+            foreach (var currentOutputField in mOutputField)
+                currentOutputField.Write(buffer);
+        }
+    }
+}
