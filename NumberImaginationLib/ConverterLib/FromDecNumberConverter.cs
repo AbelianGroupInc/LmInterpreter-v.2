7005d2a5c4a062ca1d5b961e79a8c15f18a6073f
AndrewEremeew <shaylin3@gmail.com>
2016-01-21 13:26:39 +0200

    Бяка

--- NumberImaginationLib/ConverterLib/FromDecNumberConverter.cs
+++ NumberImaginationLib/ConverterLib/FromDecNumberConverter.cs
@@ -3,18 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
 namespace NumberImagination.ConverterLib
 {
-    public abstract class FromDecNumberConverter : IFromDecNumberConverter
+    class FromDecNumberConverter
     {
-        protected void CheckNumberCorrectness(string number)
-        {
-            Regex decNumbRegExp = new Regex(@"^-?[0-9]+$");
-            if (!decNumbRegExp.IsMatch(number))
-                throw new ConverterException(ConverterExceptions.InvalidDecNumber);
-        }
-        public abstract string Convert(string number);
     }
 }
