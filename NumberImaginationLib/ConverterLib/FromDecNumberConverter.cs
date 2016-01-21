cb99da1732935c538694e9c7da5734a32d37a13d
AndrewEremeew <shaylin3@gmail.com>
2016-01-21 13:34:17 +0200

    Byaka2

--- NumberImaginationLib/ConverterLib/FromDecNumberConverter.cs
+++ NumberImaginationLib/ConverterLib/FromDecNumberConverter.cs
@@ -16,5 +16,39 @@ namespace NumberImagination.ConverterLib
                 throw new ConverterException(ConverterExceptions.InvalidDecNumber);
         }
         public abstract string Convert(string number);
+        
+        public bool IsMinusNumber(string number)
+        {
+            return number.First() == '-' ? true : false;
+        }
+
+        public string GetMantissa(string number)
+        {
+            string mantiss = String.Empty;
+            if (IsMinusNumber(number))
+            {
+                // 1 - the beginning of number mantiss
+                mantiss = number.Substring(1, number.Length - 1);
+            }
+            else
+                mantiss = number;
+
+            return mantiss;
+        }
+
+        // Returns number with added nonsignificant zeroes
+        public string AddNonsignificantZeroes(string number, int newBitCapacity)
+        {
+            TestForBitCapacity(number, newBitCapacity);
+
+            String zeroes = new String('0', newBitCapacity - number.Length);
+
+            return zeroes + number;
+        }
+        public void TestForBitCapacity(string number, int newBitCapacity)
+        {
+            if (newBitCapacity <= number.Length)
+                throw new ConverterException(ConverterExceptions.InvalidBitCapacity);
+        }
     }
 }
