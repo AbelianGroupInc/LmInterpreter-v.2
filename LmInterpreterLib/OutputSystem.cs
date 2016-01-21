1dcabc447d4d269a361108e3cdbfbfe352cf8b14
Grigoryan Artem <artem.thebest@mail.ru>
2016-01-21 14:04:41 +0200

    Added class LmInterpreter

--- LmInterpreterLib/OutputSystem.cs
+++ LmInterpreterLib/OutputSystem.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace LmInterpreterLib
 {
     public class OutputSystem
     {
-        private List<IOutputField> mOutputField;
+        private List<IOutputField> mOutputField = new List<IOutputField>();
 
         public void Add(IOutputField outputField)
         {
