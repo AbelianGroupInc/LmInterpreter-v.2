c9283b7b32de38bd2499a286ee5ed4d7b1d7b117
Grigoryan Artem <artem.thebest@mail.ru>
2016-01-21 14:19:27 +0200

    Addded class Cleaner

---
+++ LearningMachineLib/Cleaner.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LearningMachineLib
+{
+    public static class Cleaner
+    {
+        public static string Clean(string program)
+        {
+            string cleanProgram = program.Replace('\t', ' ');
+            cleanProgram = Regex.Replace(cleanProgram, @" +", " ");
+            cleanProgram = Regex.Replace(cleanProgram, @";.*?\r\n", "\r\n");
+
+            return cleanProgram;
+        }
+    }
+}
