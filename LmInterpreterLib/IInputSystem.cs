5aacba12410ffe2e75c88e913cc0215870271dca
Grigoryan Artem <artem.thebest@mail.ru>
2016-01-21 13:45:26 +0200

    Added the InputSystem interface

---
+++ LmInterpreterLib/IInputSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LmInterpreterLib
+{
+    interface IInputSystem
+    {
+        string GetBuffer();
+        void Write(string buffer);
+    }
+}
