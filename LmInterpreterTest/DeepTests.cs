81104ce9683bb0be0bd47252afe2a9be1f2e9bc7
Vitaliyvin <vitaliybondarenk@gmail.com>
2016-01-22 12:49:38 +0200

    Make tests for cleaner

---
+++ LmInterpreterTest/DeepTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using LearningMachineLib;
+
+namespace LmInterpreterTest
+{
+    [TestClass]
+    public class DeepTests
+    {
+        [TestMethod]
+        public void ClearTest()
+        {
+            //Arrange
+            StreamReader input = new StreamReader(@"../../../LmInterpreterTest\TextFile\input.txt");
+            StreamReader output = new StreamReader(@"../../../LmInterpreterTest\TextFile\output.txt");
+            string inputFile = input.ReadToEnd();
+            string outputFile = output.ReadToEnd();
+            
+            //Act
+            inputFile = Cleaner.Clean(inputFile);
+
+            //Assert
+            Assert.AreEqual(outputFile, inputFile);
+        }
+    }
+}
