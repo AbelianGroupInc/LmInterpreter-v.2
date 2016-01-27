6ce58c8ef37cb714026adc5c712218644718f653
Grigoryan Artem <artem.thebest@mail.ru>
2016-01-27 16:59:51 +0200

    LM3 scanners is added

---
+++ LM3Scanners/SyntaxException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LM3Scanners
+{
+    public class SyntaxException : Exception
+    {
+        private int mRow;
+        private int mСolumn;
+
+        public SyntaxException(string message) : base(message) { }
+
+        public SyntaxException(string message, int row, int column) : base(message)
+        {
+            mRow = row;
+            mСolumn = column;
+        }
+
+        public int Row
+        {
+            get
+            {
+                return mRow;
+            }
+        }
+
+        public int Column
+        {
+            get
+            {
+                return mСolumn;
+            }
+        }
+    }
+}
