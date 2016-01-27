2cc97b09ebc4e3a59f7a688607d42d10d385e589
AndrewEremeew <shaylin3@gmail.com>
2016-01-27 15:04:51 +0200

    Added stubs for LM3memory

---
+++ LearningMachine3/LM3Memory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningMachine3
+{
+    // Memory organisation of LM 3
+    class LM3Memory
+    {
+        private int cLM3MemoryVolume = 65536;
+        private List<IMemoryCell> mMemoryCellList;
+
+        public LM3Memory()
+        {
+            mMemoryCellList = new List<IMemoryCell>(cLM3MemoryVolume);
+            
+            for(int i = 0; i < cLM3MemoryVolume; i++)
+                mMemoryCellList.Add(new EmptyCell());
+        }
+
+        public IMemoryCell GetCellByAddress(int address)
+        {
+            throw new NotImplementedException();
+            // Need to add test for adderss correctness
+
+            return mMemoryCellList[address];
+        }
+
+        public List<IMemoryCell> GetWholeMemory()
+        {
+            return mMemoryCellList;
+        }
+    }
+}
---
+++ LearningMachine3/LearningMachine3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningMachine3
+{
+    // Implementation of Learning machine 3
+    class LearningMachine3
+    {
+        // Lm3 memory
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Need parametr - program
+        public void Fill()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Returns IMemoryOrganisation
+        public void GetMemorySection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
