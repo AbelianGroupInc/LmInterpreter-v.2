919cc31d91d92a5b5a1bfab16ac9c7317807cb07
AndrewEremeew <shaylin3@gmail.com>
2016-01-29 15:49:12 +0200

    Variable is Added

--- LearningMachineLib/Variable.cs
+++ LearningMachineLib/Variable.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using LmInterpreterLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace LearningMachineLib
 {
-    // Stub
     public class Variable : IMemoryCell
     {
+        private BigInteger mValue;
+
+        public Variable(BigInteger value)
+        {
+            mValue = value;
+        }
+
+        public BigInteger Value
+        {
+            get
+            {
+                return mValue;
+            }
+        }
+
+        public override string ToString()
+        {
+            return mValue.ToString();
+        }
     }
 }
