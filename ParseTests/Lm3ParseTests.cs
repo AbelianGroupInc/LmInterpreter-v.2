83db808905af9c3e5ef6299aaa7ba08ef311b7aa
Grigoryan Artem <artem.thebest@mail.ru>
2016-01-30 16:45:51 +0200

    LM3Parse Test is added

---
+++ ParseTests/Lm3ParseTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using LearningMachineLib;
+using LearningMachineLib.LearningMachine3;
+
+namespace ParseTests
+{
+    [TestClass]
+    public class Lm3ParseTests
+    {
+        [TestMethod]
+        public void ParseTest1()
+        {
+            string input = new StreamReader(@"../../../ParseTests\TextFiles\input.txt").ReadToEnd();
+            string output = new StreamReader(@"../../../ParseTests\TextFiles\output.txt").ReadToEnd();
+
+            LmParser parser = new LM3Parser(@"../../../ParseTests\TextFiles\testOutput.txt");
+            parser.Parse(input);
+
+            string testOutput = new StreamReader(@"../../../ParseTests\TextFiles\testOutput.txt").ReadToEnd();
+            Assert.AreEqual(output, testOutput);
+        }
+    }
+}
